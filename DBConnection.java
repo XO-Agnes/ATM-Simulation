dc724d1ddaa3debddb3e382bf63507f33d206b77
XO-Agnes <agnesgeorge2007@gmail.com>
2025-12-23 16:46:54 +0530

    Security: move DB credentials to properties file and remove hardcoding

--- DBConnection.java
+++ DBConnection.java
@@ -1,11 +1,20 @@
 import java.sql.*;
-public class DBConnection 
-{
-    public static Connection getConnection() throws Exception
-    {
-        String url = "jdbc:mysql://localhost:3306/atm_db";
-        String user = "root";
-        String password = "2704";
-        return DriverManager.getConnection(url,user,password);
+import 
+java.util.*;
+import java.io.FileInputStream;
+
+public class DBConnection {
+
+    public static Connection getConnection() throws Exception {
+
+        Properties props = new Properties();
+        FileInputStream fis = new FileInputStream("db.properties");
+        props.load(fis);
+
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+
+        return DriverManager.getConnection(url, user, password);
     }
 }
